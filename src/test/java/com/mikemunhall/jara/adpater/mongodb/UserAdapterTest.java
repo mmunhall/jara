1c00a53271afce0789fa71eeeed667d711265b13
Mike Munhall <munhall.mike@gmail.com>
2012-12-04 11:11:17 -0700

    Made UserAdaterTest pass.

--- src/test/java/com/mikemunhall/jara/adpater/mongodb/UserAdapterTest.java
+++ src/test/java/com/mikemunhall/jara/adpater/mongodb/UserAdapterTest.java
@@ -1,12 +1,44 @@
 package com.mikemunhall.jara.adpater.mongodb;
 
+import com.mikemunhall.jara.adapter.mongodb.UserAdapter;
+import com.mikemunhall.jara.model.User;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
+import org.bson.types.ObjectId;
+import org.joda.time.DateTime;
 import org.testng.annotations.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
 import static org.testng.AssertJUnit.*;
 
 public class UserAdapterTest {
 
+    private UserAdapter userAdapter = new UserAdapter();
+
     @Test
     public void adapt_ProperlyBindsProperties() {
-        fail("Not implemented");
+        BasicDBObject obj = (BasicDBObject) new BasicDBObjectBuilder().
+            add("_id", new ObjectId()).
+            add("firstName", "Bitsy").
+            add("lastName", "Spider").
+            add("username", "bspider").
+            add("password", "wat3rsp0ut").
+            add("email", "downcametherain@gmail.com").
+            add("createDate", new DateTime(2012, 2, 12, 0, 0, 0).toDate()).
+            get();
+
+        User user = userAdapter.adapt(obj);
+
+        assertEquals("Bitsy", user.getFirstName());
+        assertEquals("Spider", user.getLastName());
+        assertEquals("bspider", user.getUsername());
+        assertEquals("wat3rsp0ut", user.getPassword());
+        assertEquals("downcametherain@gmail.com", user.getEmail());
+        assertEquals(2012, user.getCreateDate().getYear());
+        assertEquals(2, user.getCreateDate().getMonthOfYear());
+        assertEquals(12, user.getCreateDate().getDayOfMonth());
     }
 }
