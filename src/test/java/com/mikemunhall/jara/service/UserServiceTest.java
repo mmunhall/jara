08a96f13889d2e1ed88ce8fe257a56ff3afa79dd
Mike Munhall <munhall.mike@gmail.com>
2012-12-04 14:28:38 -0700

    UserService tests.

--- src/test/java/com/mikemunhall/jara/service/UserServiceTest.java
+++ src/test/java/com/mikemunhall/jara/service/UserServiceTest.java
@@ -1,17 +1,49 @@
 package com.mikemunhall.jara.service;
 
+import com.mikemunhall.jara.dao.mongodb.UserDao;
+import com.mikemunhall.jara.model.User;
 import org.testng.annotations.*;
+import static org.mockito.Mockito.*;
 import static org.testng.AssertJUnit.*;
 
 public class UserServiceTest {
 
-    @Test
-    public void getUserByUsername_ValidUser_ReturnsUser() {
-        fail("Not implemented");
+    private UserDao userDao;
+    private UserService userService;
+
+    @BeforeTest
+    public void setUpUserServiceAndDependencies() {
+        userDao = mock(UserDao.class);
+        userService = new UserService(userDao);
     }
 
     @Test
+    public void getUserByUsername_ValidUser_ReturnsUser() {
+        when(userDao.getUserByUsername(anyString())).thenReturn(new User());
+
+        User user = null;
+
+        try {
+            user = userService.getUserByUsername("un1");
+        } catch (Exception e) {
+            fail("Exception thrown, but not expected: " + e.getMessage());
+        }
+
+        assertNotNull(user);
+        assert(user instanceof User);
+    }
+
     public void getUserByUsername_InvalidUser_ThrowsInvalidUserException() {
-        fail("Not implemented");
+        when(userDao.getUserByUsername(anyString())).thenReturn(null);
+
+        try {
+            User user = userService.getUserByUsername("un1");
+            fail("Expected InvalidUserException");
+        } catch (InvalidUserException e) {
+            // passes
+        } catch (Exception e) {
+            fail("Unexpected exception type: " + e.getClass());
+        }
+
     }
 }
