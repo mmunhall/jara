f0dba75e7759d309265a77d2b0fc7a30381cabe8
Mike Munhall <munhall.mike@gmail.com>
2013-01-05 17:14:40 -0700

    Test for recipe service create method.

---
+++ src/test/java/com/mikemunhall/jara/service/RecipeServiceTest.java
@@ -0,0 +1,31 @@
+package com.mikemunhall.jara.service;
+
+import com.mikemunhall.jara.dao.mongodb.RecipeDao;
+import com.mikemunhall.jara.model.Recipe;
+import org.bson.types.ObjectId;
+import org.testng.annotations.*;
+import static org.mockito.Mockito.*;
+import static org.testng.AssertJUnit.*;
+
+public class RecipeServiceTest {
+
+    private RecipeDao recipeDao;
+    private RecipeService recipeService;
+
+    @BeforeClass
+    public void setUpRecipeServiceAndDependencies() {
+        recipeDao = mock(RecipeDao.class);
+        recipeService = new RecipeService(recipeDao);
+    }
+
+    @Test
+    public void create_ReturnsRecipeWithGivenUser() {
+        ObjectId objId = new ObjectId();
+        when(recipeDao.save(any(Recipe.class))).thenReturn(new Recipe(new ObjectId().toString()));
+
+        Recipe recipe = recipeService.create(objId.toString());
+
+        assert(recipe instanceof Recipe);
+        assertEquals(objId.toString(), recipe.getUserId());
+    }
+}
